a2cde65a30b3f362e70681f3abb8ec32773bb266
iVGeek <48223278+iVGeek@users.noreply.github.com>
2024-09-24 00:24:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,5 @@ document.getElementById("current-year").textContent = new Date().getFullYear();
 // Optional: Handle form submission (for demonstration)
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    const name = document.getElementById("name").value;
-    const message = `Thank you for your message, ${name}!`;
-    alert(message);
-    // Additional glow effect when form is submitted
-    document.getElementById("contact").style.boxShadow = "0 0 15px rgba(106, 13, 173, 1)";
+    alert("Thank you for your message, " + document.getElementById("name").value + "!");
 });
